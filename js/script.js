d057b32beb21d3dc1ba21583a08841ede123ee69
Frameck <fra9879@gmail.com>
2021-11-02 15:53:04 +0100

    Completed exercise

--- js/script.js
+++ js/script.js
@@ -5,5 +5,20 @@ const vueApp = new Vue({
     data: {
         newTask: '',
         tasksList: ['Fare la spesa', 'Andare in banca', 'Comprare il pane']
+    },
+    methods: {
+        addTaskOnClick() {
+            const duplicatedElement = this.tasksList.find(el => this.newTask.trim().toLowerCase() === el.toLowerCase())
+            
+            // se il nuovo task è una stringa vuota o se esiste già non eseguo il push
+            if (this.newTask.trim() === '' || duplicatedElement !== undefined) {
+                return
+            }
+            this.tasksList.push(this.newTask.trim())
+            this.newTask = ''
+        },
+        removeTaskOnClick(i) {
+            this.tasksList.splice(i, 1)
+        }
     }
 })
