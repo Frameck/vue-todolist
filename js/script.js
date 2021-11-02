e4d85e0671c8b22de4a623fb2a00b26bfc25e141
Frameck <fra9879@gmail.com>
2021-11-02 15:00:00 +0100

    Display tasks from Vue with v-for

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ Vue.config.devtools = true
 const vueApp = new Vue({
     el: '#app',
     data: {
-        
+        newTask: '',
+        tasksList: ['Fare la spesa', 'Andare in banca', 'Comprare il pane']
     }
 })
